0cf3bb9497611c327152927adcc08a82caa19ae9
Pedro Lana <pedroalana@hotmail.com>
2021-04-26 13:06:53 -0300

    Upping all files to github

---
+++ resources/views/layout/layout.blade.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="{{ config('app.locale')}}">
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    <link rel="stylesheet" href="/css/layout.css">
+    <meta charset="UTF-8">
+    <title>Venda de produtos</title>
+</head>
+<body>
+
+    <header>
+        <div class="logo-b">
+            <a href="/">
+                <img src="img/buyall_logo_simple_transp.png" class="logo-img" alt="">
+            </a>
+        </div>
+        <div class="menu bg-primary">
+        </div>
+    </header>
+    <div class="general">
+        <div class="content">
+            <div class="products container-fluid">
+                <div class="row ">
+                    <div class="product col-md-3 ">
+                        <img src="img/mulheres.jpg" alt="">
+                        <div class="prod-inf">
+                        <h1>Nome da loja</h1>
+                        <h2>Tipo da loja</h2>
+                        <p>Vendedor</p>
+                        </div>
+                    </div>
+
+                    <div class="product col-md-3">
+                        <img src="img/platao.jpg" alt="">
+                        <div class="prod-inf">
+                        <h1>Nome da loja</h1>
+                        <h2>Tipo da loja</h2>
+                        <p>Vendedor</p>
+                        </div>
+                    </div>
+
+                    <div class="product col-md-3">
+                        <img src="img/poder.jpg" alt="">
+                        <div class="prod-inf">
+                        <h1>Nome da loja</h1>
+                        <h2>Tipo da loja</h2>
+                        <p>Vendedor</p>
+                        </div>
+                    </div>
+                    <div class="product col-md-3">
+                        <img src="img/poder.jpg" alt="">
+                        <div class="prod-inf">
+                        <h1>Nome da loja</h1>
+                        <h2>Tipo da loja</h2>
+                        <p>Vendedor</p>
+                        </div>
+                    </div>
+                    <div class="product col-md-3">
+                        <img src="img/poder.jpg" alt="">
+                        <div class="prod-inf">
+                        <h1>Nome da loja</h1>
+                        <h2>Tipo da loja</h2>
+                        <p>Vendedor</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <footer>
+        <div class="rights">
+            <p>Todos os direitos reservados a Pedro Lana</p>
+        </div>
+    </footer>
+</body>
+</html>
